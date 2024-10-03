a6a68807c547dbf5c8191f47b179e2998ebc877d
Jack <jmcphillips469@gmail.com>
2024-10-03 11:15:54 -0500

    adding client list to main.py to allow for multiple clients to connect

--- backend/main.py
+++ backend/main.py
@@ -2,38 +2,37 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import asyncio
 
 app = FastAPI()
+connectedClients = set()
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     print("Waiting for Connection")
-    
+    connectedClients.add(websocket)
+
     try:
         await websocket.accept()
         print("Connection accepted")
-    except asyncio.TimeoutError:
-        print("Connection timed out")
-        return
-    except OSError as e:
-        print(f"Network error occurred: {e}")
-        return
-    except Exception as e:
-        print(f"Error Occurred: {e}")
-        return 
+        
+        while True:
+            try:
+                data = await websocket.receive_text()
+                print(f"Received from client: {data}")
+                for client in connectedClients:
+                    await client.send_text(f"Message Received: {data}")
+            except WebSocketDisconnect:
+                print("Client disconnected")
+                break
+            except asyncio.TimeoutError:
+                print("Timeout during communication")
+                break
+            except OSError as e:
+                print(f"Network error during communication: {e}")
+                break
+            except Exception as e:
+                print(f"Error occurred during communication: {e}")
+                break
+    finally:
+        connectedClients.remove(websocket)
+        await websocket.close()
+        print("Connection closed")
 
-    while True:
-        try:
-            data = await websocket.receive_text()
-            await websocket.send_text(f"Message Received: {data}")
-            print(f"Received from client: {data}")
-        except WebSocketDisconnect:
-            print("Client disconnected")
-            break
-        except asyncio.TimeoutError:
-            print("Timeout during communication")
-            break
-        except OSError as e:
-            print(f"Network error during communication: {e}")
-            break
-        except Exception as e:
-            print(f"Error occurred during communication: {e}")
-            break
