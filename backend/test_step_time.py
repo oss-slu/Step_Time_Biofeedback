779f66b85e350c88c9186f660b809e7e0d1417dd
Colin Seper <sepercolin@gmail.com>
2024-10-13 22:44:06 -0500

    Added Paramaterization to Step Time Calculation Function
    Added an optional input moving_avg_factor with a default of 2. Created tests for checking a non-default threshold and non-default moving_avg_factor

--- backend/test_step_time.py
+++ backend/test_step_time.py
@@ -8,6 +8,20 @@ class TestCalculateStepTime(unittest.TestCase):
         force_data = [(0.0, 0), (0.1, 0), (0.2, 10), (0.3, 15)]
         step_times = calculate_step_time(force_data)
         self.assertEqual(step_times, [])  # Expected: []
+    
+    def test_step_time_below_non_default_threshold(self):
+        force_data = [(0.0, 0), (0.1, 10), (0.2, 20), (0.3, 25)]
+        step_times = calculate_step_time(force_data, 30)
+        self.assertEqual(step_times, [])
+    
+    def test_step_time_at_or_above_non_default_threshold(self):
+        force_data = [
+            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 25),  
+            (0.4, 0), (0.5, 30), (0.6, 40), (0.7, 15)   
+        ]
+        step_times = calculate_step_time(force_data, 30)
+        self.assertAlmostEqual(step_times[0], 0.1, places=2)
+        self.assertAlmostEqual(step_times[1], 0.2, places=2)
      
     def test_step_time_calculation(self):
         # Adjusted force data
@@ -29,3 +43,12 @@ class TestCalculateStepTime(unittest.TestCase):
         # Check if the step times are approximately equal to expected values
         self.assertAlmostEqual(step_times[0], 0.2, places = 2)
         self.assertAlmostEqual(step_times[1], 0.2, places = 2) 
+    
+    def test_non_default_moving_avg_factor(self):
+        force_data = [
+            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 25),  
+            (0.4, 0), (0.5, 30), (0.6, 40), (0.7, 15)   
+        ]
+        step_times = calculate_step_time(force_data, 30)
+        self.assertAlmostEqual(step_times[0], 0.1, places=2)
+        self.assertAlmostEqual(step_times[1], 0.2, places=2)
