66c746d9c91ce793be0be6b9aa2bee7597e36e65
Colin Seper <sepercolin@gmail.com>
2024-10-20 19:07:47 -0500

    Update StepTime Calculation Function
    Updated Step Time Calculation function to properly return the moving averages (except in the case where the moving average factor is less than 2, in which case we return the step time values) and the tests for the function accordingly.

--- backend/Step_Time_Calculation.py
+++ backend/Step_Time_Calculation.py
@@ -27,9 +27,8 @@ def calculate_step_time(force_data, threshold=20.0, moving_avg_factor=2):
             step_times.append(step_time)
             step_start_time = None  # Reset for the next step
     
-    # return before calculating moving_avg -> MAY BE REMOVED... waiting for clarification 
     if moving_avg_factor < 2:
-        return step_time
+        return step_times
 
     for i in range(len(step_times)):
         if i < moving_avg_factor - 1:
@@ -38,5 +37,5 @@ def calculate_step_time(force_data, threshold=20.0, moving_avg_factor=2):
             moving_average = np.mean(step_times[i - moving_avg_factor + 1:i+1])  
             step_time_moving_averages.append(moving_average)  
             
-    return step_times
+    return step_time_moving_averages
 
--- backend/test_step_time.py
+++ backend/test_step_time.py
@@ -3,52 +3,42 @@ from Step_Time_Calculation import calculate_step_time
 
 class TestCalculateStepTime(unittest.TestCase):
 
-    def test_step_time_below_threshold(self):
+    def test_step_time_avgs_below_threshold(self):
         # All values below threshold should yield no step times
         force_data = [(0.0, 0), (0.1, 0), (0.2, 10), (0.3, 15)]
-        step_times = calculate_step_time(force_data)
-        self.assertEqual(step_times, [])  # Expected: []
+        step_time_moving_averages = calculate_step_time(force_data)
+        self.assertEqual(step_time_moving_averages, [])  # Expected: []
     
-    def test_step_time_below_non_default_threshold(self):
-        force_data = [(0.0, 0), (0.1, 10), (0.2, 20), (0.3, 25)]
-        step_times = calculate_step_time(force_data, 30)
-        self.assertEqual(step_times, [])
-    
-    def test_step_time_at_or_above_non_default_threshold(self):
+    def test_step_time_avgs_at_or_above_threshold(self):
         force_data = [
-            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 25),  
-            (0.4, 0), (0.5, 30), (0.6, 40), (0.7, 15)   
+            (0.0, 0), (0.1, 30), (0.2, 25), (0.3, 35),  
+            (0.4, 40), (0.5, 20), (0.6, 10), (0.7, 0)   
         ]
-        step_times = calculate_step_time(force_data, 30)
-        self.assertAlmostEqual(step_times[0], 0.1, places=2)
-        self.assertAlmostEqual(step_times[1], 0.2, places=2)
+        step_time_moving_averages = calculate_step_time(force_data, 30)
+        self.assertAlmostEqual(step_time_moving_averages[0], 0.1, places=2)
+        self.assertAlmostEqual(step_time_moving_averages[1], 0.15, places=2)
      
-    def test_step_time_calculation(self):
-        # Adjusted force data
+    def test_moving_avg_factor_below_two(self):
         force_data = [
-            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 0),  # First step
-            (0.4, 0), (0.5, 25), (0.6, 30), (0.7, 0)   # Second step
+            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 0),  
+            (0.4, 0), (0.5, 25), (0.6, 30), (0.7, 0)   
         ]
-        step_times = calculate_step_time(force_data)
-        # Check if the step times are approximately equal to expected values
+        step_times = calculate_step_time(force_data, moving_avg_factor=1)
         self.assertAlmostEqual(step_times[0], 0.2, places = 2)
         self.assertAlmostEqual(step_times[1], 0.2, places = 2) 
 
-    def test_moving_average(self):
+    def test_moving_avg_factor_above_two(self):
         force_data = [
-            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 0),
-            (0.4, 0), (0.5, 25), (0.6, 30), (0.7, 0)
+            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 0),  # 0.2 
+            (0.4, 40), (0.5, 45), (0.6, 50), (0.7, 0), # 0.3
+            (0.8, 20), (0.9, 25), (1.0, 0), (1.1, 0),  # 0.2
+            (1.2, 40), (1.3, 45), (1.4, 50), (1.5, 0), # 0.2
         ]
-        step_times = calculate_step_time(force_data)
-        # Check if the step times are approximately equal to expected values
-        self.assertAlmostEqual(step_times[0], 0.2, places = 2)
-        self.assertAlmostEqual(step_times[1], 0.2, places = 2) 
-    
-    def test_non_default_moving_avg_factor(self):
-        force_data = [
-            (0.0, 0), (0.1, 25), (0.2, 30), (0.3, 25),  
-            (0.4, 0), (0.5, 30), (0.6, 40), (0.7, 15)   
-        ]
-        step_times = calculate_step_time(force_data, 30)
-        self.assertAlmostEqual(step_times[0], 0.1, places=2)
-        self.assertAlmostEqual(step_times[1], 0.2, places=2)
+        step_time_moving_averages = calculate_step_time(force_data, moving_avg_factor=3)
+        self.assertAlmostEqual(step_time_moving_averages[0], 0.2, places=2)
+        self.assertAlmostEqual(step_time_moving_averages[1], 0.3, places=2)
+        self.assertAlmostEqual(step_time_moving_averages[2], 0.233, places=3)
+        self.assertAlmostEqual(step_time_moving_averages[2], 0.233, places=3)
+
+if __name__ == "__main__":
+    unittest.main();
