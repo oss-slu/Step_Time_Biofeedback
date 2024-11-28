ae28f6e66650b33f2880f544694783c48a8a1bf5
Onoja <onoja.jsdev@gmail.com>
2024-11-28 12:50:04 -0600

    round up target zones to 4 decimal places

--- frontend/src/StepTimeDigits.js
+++ frontend/src/StepTimeDigits.js
@@ -1,13 +1,17 @@
 import "./StepTimeDigits.css";
 
 function StepTimeDigits({ stepTime }) {
+  const minTargetZoneLeft = stepTime.targetZones.left.min
+  const minTargetZoneRight = stepTime.targetZones.right.min
+  const maxTargetZoneLeft = stepTime.targetZones.left.max
+  const maxTargetZoneRight = stepTime.targetZones.right.max
   return (
     <div data-testid='step-time-digits-view' className="StepTimeDigits">
       <div data-testid='target-zones-values' className="TargetZones">
         <p data-testid='target-zones-title'>Target Zones</p>
         <ul>
-          <li data-testid='target-zones-left'>{stepTime.targetZones.left.min}-{stepTime.targetZones.left.max}</li>
-          <li data-testid='target-zones-right'>{stepTime.targetZones.right.min}-{stepTime.targetZones.right.max}</li>
+          <li data-testid='target-zones-left'>{minTargetZoneLeft.toFixed(4)}-{maxTargetZoneLeft.toFixed(4)}</li>
+          <li data-testid='target-zones-right'>{minTargetZoneRight.toFixed(4)}-{maxTargetZoneRight.toFixed(4)}</li>
         </ul>
       </div>
       <div data-testid='current-step-time-values' className="CurrentStepTime">
