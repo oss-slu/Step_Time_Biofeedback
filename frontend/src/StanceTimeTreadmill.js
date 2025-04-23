48310a2508f200de430807c5154a23ca8aab9380
iAmNsengi <iamnsengi@icloud.com>
2025-04-23 09:14:17 +0200

    fix: layout of treadmill to fit the page && responsivenes

--- frontend/src/StanceTimeTreadmill.js
+++ frontend/src/StanceTimeTreadmill.js
@@ -7,17 +7,21 @@ function StanceTimeTreadmill({ stanceTime }) {
 
   const XFontSize = 8;
   const xColor = {
-    left:'black',
-    right:'black'
-  }
+    left: "black",
+    right: "black",
+  };
 
   const scaleFactor = 50;
   const leftScaleFactor = scaleFactor / stanceTime.left;
   const rightScaleFactor = scaleFactor / stanceTime.right;
-  const leftOffsetMin = (stanceTime.left - stanceTime.targetZones.left.min) * -leftScaleFactor;
-  const leftOffsetMax = (stanceTime.left - stanceTime.targetZones.left.max) * leftScaleFactor;
-  const rightOffsetMin = (stanceTime.right - stanceTime.targetZones.right.min) * -rightScaleFactor;
-  const rightOffsetMax = (stanceTime.right - stanceTime.targetZones.right.max) * rightScaleFactor;
+  const leftOffsetMin =
+    (stanceTime.left - stanceTime.targetZones.left.min) * -leftScaleFactor;
+  const leftOffsetMax =
+    (stanceTime.left - stanceTime.targetZones.left.max) * leftScaleFactor;
+  const rightOffsetMin =
+    (stanceTime.right - stanceTime.targetZones.right.min) * -rightScaleFactor;
+  const rightOffsetMax =
+    (stanceTime.right - stanceTime.targetZones.right.max) * rightScaleFactor;
 
   // Based on a 100x100 viewbox, ensures at least 1 target-zone is visible to imply that the other is too far
   let leftMaxPosition = Math.max(scaleFactor - leftOffsetMin, 0);
@@ -33,7 +37,6 @@ function StanceTimeTreadmill({ stanceTime }) {
   if (!stanceTime.left) leftCurrent = scaleFactor;
   if (!stanceTime.right) rightCurrent = scaleFactor;
 
-
   function hideHelp() {
     if (showHelpText) {
       setShowHelpText(false);
@@ -43,10 +46,10 @@ function StanceTimeTreadmill({ stanceTime }) {
   // bounds check
   let isInsideZone = {
     left: false,
-    right: false
-  }
+    right: false,
+  };
 
-  if (stanceTime.right){
+  if (stanceTime.right) {
     if (rightCurrent <= rightMaxPosition && rightCurrent >= rightMinPosition) {
       isInsideZone.right = true;
     } else {
@@ -63,14 +66,21 @@ function StanceTimeTreadmill({ stanceTime }) {
   }
 
   return (
-    <div data-testid="stance-time-treadmill-view" className="StanceTimeTreadmill" onClick={hideHelp}>
+    <div
+      data-testid="stance-time-treadmill-view"
+      className="StanceTimeTreadmill"
+      onClick={hideHelp}
+    >
       <div className="stance-time-treadmill-container">
         <div
           className="help-icon-container"
           onClick={() => setShowHelpText(true)}
           title="Show help"
         >
-          <HelpCircle className="text-gray-400 hover:text-gray-300 cursor-pointer" size={30} />
+          <HelpCircle
+            className="text-gray-400 hover:text-gray-300 cursor-pointer"
+            size={30}
+          />
         </div>
 
         {showHelpText && (
@@ -80,20 +90,35 @@ function StanceTimeTreadmill({ stanceTime }) {
               <ul>
                 <li>
                   <strong>Stance Time Range:</strong>
-                  The <span style={{ backgroundColor: "#a3bed4", padding: "2px 6px", borderRadius: "3px" }}>blue rectangles</span> represents the stance time range bounds for the left and right foot.
+                  The{" "}
+                  <span
+                    style={{
+                      backgroundColor: "#a3bed4",
+                      padding: "2px 6px",
+                      borderRadius: "3px",
+                    }}
+                  >
+                    blue rectangles
+                  </span>{" "}
+                  represents the stance time range bounds for the left and right
+                  foot.
                 </li>
 
                 <li>
-                  <strong>X's and O's: </strong>
-                  A <span style={{ color: "green", fontSize: "2rem" }}>●</span> appears if the step is within range; otherwise, a
-                  <span style={{ color: "red", fontSize: "1.1rem" }}> X</span> shows the step was out of range. A black <span style={{ color: "black", fontSize: "1.1rem" }}> X</span> appears if no stance time is found.
+                  <strong>X's and O's: </strong>A{" "}
+                  <span style={{ color: "green", fontSize: "2rem" }}>●</span>{" "}
+                  appears if the step is within range; otherwise, a
+                  <span style={{ color: "red", fontSize: "1.1rem" }}> X</span>{" "}
+                  shows the step was out of range. A black{" "}
+                  <span style={{ color: "black", fontSize: "1.1rem" }}> X</span>{" "}
+                  appears if no stance time is found.
                 </li>
 
                 <li>
                   <strong>Baseline:</strong>
-                  The horizontal dashed line ----- represents the normal stance time position.
+                  The horizontal dashed line ----- represents the normal stance
+                  time position.
                 </li>
-
               </ul>
               <button
                 onClick={() => setShowHelpText(false)}
@@ -104,60 +129,94 @@ function StanceTimeTreadmill({ stanceTime }) {
             </div>
           </div>
         )}
+
+        <div className="treadmill-wrapper">
+          <svg
+            data-testid="treadmill-svg"
+            className="Treadmill"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+          >
+            <g data-testid="target-zones" className="TargetZones">
+              <g data-testid="baseline" className="YAxis">
+                <line x1="5" x2="5" y1="0" y2="100" />
+              </g>
+              <g data-testid="left-target-zones" className="LeftTargetZones">
+                <rect
+                  className="max-zone"
+                  x="24"
+                  y={leftMinPosition}
+                  width="10"
+                  height={leftMaxPosition - leftMinPosition}
+                />
+              </g>
+              <g data-testid="right-target-zones" className="RightTargetZones">
+                <rect
+                  className="max-zone"
+                  x="74"
+                  y={rightMinPosition}
+                  width="10"
+                  height={rightMaxPosition - rightMinPosition}
+                />
+              </g>
+              <g data-testid="baseline" className="Baseline">
+                <line x1="5" x2="90" y1="50" y2="50" />
+              </g>
+            </g>
+            <g data-testid="stance-time-values" className="StanceTimeValues">
+              <g
+                data-testid="left-step-mark"
+                className="LeftStep"
+                style={{
+                  transform: `translate(0, ${leftCurrent}px)`,
+                  transition: "transform 0.25s ease-in-out",
+                }}
+              >
+                {isInsideZone.left ? (
+                  <circle cx="29" cy="0" r="2" fill="green" />
+                ) : (
+                  <text
+                    x="29"
+                    y={XFontSize / 4}
+                    fontSize={XFontSize}
+                    textAnchor="middle"
+                    fill={xColor.left}
+                  >
+                    x
+                  </text>
+                )}
+              </g>
+              <g
+                data-testid="right-step-mark"
+                className="RightStep"
+                style={{
+                  transform: `translate(0, ${rightCurrent}px)`,
+                  transition: "transform 0.25s ease-in-out",
+                }}
+              >
+                {isInsideZone.right ? (
+                  <circle cx="79" cy="0" r="2" fill="green" />
+                ) : (
+                  <text
+                    x="79"
+                    y={XFontSize / 4}
+                    fontSize={XFontSize}
+                    textAnchor="middle"
+                    fill={xColor.right}
+                  >
+                    x
+                  </text>
+                )}
+              </g>
+            </g>
+          </svg>
+          <div className="treadmill-labels">
+            <span className="label-left">Left</span>
+            <span className="label-right">Right</span>
+          </div>
+        </div>
       </div>
-
-      <svg data-testid="treadmill-svg" className="Treadmill" viewBox="0 0 100 100" preserveAspectRatio="none">
-        <g data-testid="target-zones" className="TargetZones">
-          <g data-testid="baseline" className="YAxis">
-            <line x1="5" x2="5" y1="0" y2="100" />
-          </g>
-          <g data-testid="left-target-zones" className="LeftTargetZones">
-            <rect className="max-zone" x="24" y={leftMinPosition} width="10" height={leftMaxPosition - leftMinPosition} />
-          </g>
-          <g data-testid="right-target-zones" className="RightTargetZones">
-            <rect className="max-zone" x="74" y={rightMinPosition} width="10" height={rightMaxPosition - rightMinPosition} />
-          </g>
-          <g data-testid="baseline" className="Baseline">
-            <line x1="5" x2="90" y1="50" y2="50" />
-          </g>
-        </g>
-        <g data-testid="stance-time-values" className="StanceTimeValues">
-          <g
-            data-testid="left-step-mark" className="LeftStep"
-            style={{
-              transform: `translate(0, ${leftCurrent}px)`,
-              transition: 'transform 0.25s ease-in-out',
-            }}
-          >
-            {isInsideZone.left ? (
-              <circle cx="29" cy="0" r="2" fill="green" />
-            ) : (
-              <text x="29" y={XFontSize/4} fontSize={XFontSize} textAnchor="middle" fill={xColor.left}>x</text>
-            )}
-
-          </g>
-          <g
-            data-testid="right-step-mark" className="RightStep"
-            style={{
-              transform: `translate(0, ${rightCurrent}px)`,
-              transition: 'transform 0.25s ease-in-out',
-            }}
-          >
-            {isInsideZone.right ? (
-              <circle cx="79" cy="0" r="2" fill="green" />
-            ) : (
-              <text x="79" y={XFontSize/4} fontSize={XFontSize} textAnchor="middle" fill={xColor.right}>x</text>
-            )}
-
-          </g>
-        </g>
-      </svg>
-
-      <svg data-testid="treadmill-text-svg" className="Treadmill-text" viewBox="0 0 100 10" preserveAspectRatio="none">
-        <text x="29" y="5" textAnchor="middle" fontSize="5" fill="white">Left</text>
-        <text x="79" y="5" textAnchor="middle" fontSize="5" fill="white">Right</text>
-      </svg>
-    </div >
+    </div>
   );
 }
 
--- frontend/src/components/ResearcherToolbar.js
+++ frontend/src/components/ResearcherToolbar.js
@@ -12,7 +12,11 @@ function ResearcherToolbar({
   const threshold_toast = useRef(null);
 
   function activateToast() {
-    threshold_toast.current.show({ severity: "success", summary: "Submitted", detail: "Threshold successfully submitted!" });
+    threshold_toast.current.show({
+      severity: "success",
+      summary: "Submitted",
+      detail: "Threshold successfully submitted!",
+    });
   }
 
   return (
@@ -20,7 +24,7 @@ function ResearcherToolbar({
       <h3 className="toolbar-header">Researcher Toolbar</h3>
 
       <div className="tool">
-        <label className="tool-label">Moving Average Factor:</label>
+        <label className="tool-label">Moving Average Factor:</label> <br />
         <input
           type="number"
           value={movingAverageFactor}
@@ -33,7 +37,7 @@ function ResearcherToolbar({
       <br />
 
       <div className="tool">
-        <label className="tool-label">Threshold:</label>
+        <label className="tool-label">Threshold:</label> <br />
         <input
           type="number"
           value={threshold}
